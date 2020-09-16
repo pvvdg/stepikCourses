a6009a71f51de731ba7ba68165c583ae9129b74c
pvvdg <vladpetrachuk@gmail.com>
2020-09-16 20:45:15 +0300

    Added solution

--- 1_10_7/main.go
+++ 1_10_7/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hi")
+	var x, p, y, year int
+	fmt.Fscan(os.Stdin, &x, &p, &y)
+	numberOfCentsInRuble := 100
+	x = x * numberOfCentsInRuble
+	y = y * numberOfCentsInRuble
+	for {
+		if x <= y {
+			x += (x * p / numberOfCentsInRuble)
+			year++
+		} else if x >= y {
+			break
+		}
+
+	}
+	fmt.Println(year)
 }
+
+/*
+Вклад в банке составляет x рублей. Ежегодно он увеличивается на p процентов, после чего дробная часть копеек
+ отбрасывается. Каждый год сумма вклада становится больше. Определите, через сколько лет вклад составит не
+ менее y рублей.
+Входные данные
+Программа получает на вход три натуральных числа: x, p, y.
+
+Выходные данные
+Программа должна вывести одно целое число.
+Sample Input:
+100
+10
+200
+Sample Output:
+8
+*/
