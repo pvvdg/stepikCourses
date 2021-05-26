0bf6384f2e1bf537d2dcce8c84142740c69c293c
pvvdg <vladpetrachuk@gmail.com>
2021-05-26 12:32:35 +0300

    Updated solution

--- 3_9_1/main.go
+++ 3_9_1/main.go
@@ -7,28 +7,23 @@ func main() {
 	ch2 := make(chan int)
 	ch3 := make(chan struct{}, 1)
 	go func() {
-		fmt.Println("In ", <-ch3)
-		ch3 <- struct{}{}
-		fmt.Println(<-ch3)
+		// ch3 <- struct{}{}
+		ch2 <- 2
 	}()
-	defer close(ch3)
 	fmt.Println(<-calculator(ch1, ch2, ch3))
-	// stopChan := make(chan struct{}, 1)
-	// go func() {
-	// fmt.Println(<-stopChan)
-	// }()
-	// fmt.Println(<-calculator(ch1, ch2, stopChan))
 }
 
 func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
 	ch := make(chan int)
 	go func() {
+		defer close(ch)
 		select {
 		case res := <-firstChan:
 			ch <- res * res
 		case res := <-secondChan:
 			ch <- res * 3
 		case <-stopChan:
+			fmt.Println("cancel func")
 			return
 		}
 	}()
